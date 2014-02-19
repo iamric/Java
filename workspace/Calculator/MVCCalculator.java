c9fd2e20871f915f990646cc30364f1dcff9ab6b
Ric Waiters <iamwaiters@yahoo.com>
2014-02-19 11:13:41 -0600

    Create MVCCalculator.java

---
+++ workspace/Calculator/MVCCalculator.java
@@ -0,0 +1,18 @@
+
+public class MVCCalculator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		CalculatorView theView = new CalculatorView();
+		
+		CalculatorModel theModel = new CalculatorModel();
+		
+		CalculatorController theController = new CalculatorController(theView, theModel);
+		
+		theView.setVisible(true);
+
+	}
+
+}
