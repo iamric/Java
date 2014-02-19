75774e15b6a5c0e862b8bad6ad685f7274aff4d9
Ric Waiters <iamwaiters@yahoo.com>
2014-02-19 11:15:33 -0600

    Create CalculatorModel.java

---
+++ workspace/Calculator/CalculatorModel.java
@@ -0,0 +1,15 @@
+
+public class CalculatorModel {
+	
+	private int calculationValue;
+	
+	public void addTwoNumber(int firstNumber, int secondNumber) {
+		
+		calculationValue = firstNumber + secondNumber;
+		
+	}
+	
+	public int getCalculationValue() {
+		return calculationValue;
+	}
+}
